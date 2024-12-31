12cc2aacbe0c2afe9e42a4b8ff678ffaa54fc92e
Stéphane Graber <stgraber@stgraber.org>
2024-12-31 17:42:27 -0500

    terraform: Use Ubuntu 22.04 as base (needed for Ceph repos)
    Signed-off-by: Stéphane Graber <stgraber@stgraber.org>

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ module "baremetal" {
 
   project_name   = "dev-incus-deploy"
   instance_names = ["server01", "server02", "server03", "server04", "server05"]
-  image          = "images:ubuntu/24.04"
+  image          = "images:ubuntu/22.04"
   memory         = "4GiB"
   storage_pool   = "default"
 }
